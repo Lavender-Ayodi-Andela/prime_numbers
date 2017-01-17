ec9f0a783e80bbfc7b34b02feace0e4501ac9a22
Lavender Ayodi <layodi749@gmail.com>
2017-01-17 14:53:45 -0800

    Second commit

--- prime_numbers.py
+++ prime_numbers.py
@@ -2,3 +2,5 @@ first_no = int(raw_input("Enter the first number: "))
 print first_no
 second_no = int(raw_input("Enter the second number: "))
 print first_no
+for num in range(first_no,second_no):
+print num
