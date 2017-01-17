dbb05ae3adf8e9ccf1fb10ab4000e35e03a89163
Lavender Ayodi <layodi749@gmail.com>
2017-01-17 14:56:37 -0800

    third commit

--- prime_numbers.py
+++ prime_numbers.py
@@ -3,4 +3,8 @@ print first_no
 second_no = int(raw_input("Enter the second number: "))
 print first_no
 for num in range(first_no,second_no):
-print num
+	for a in range(2,num):
+        if (num%a==0):
+            break
+    else:
+        print(num)
