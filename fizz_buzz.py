3b6dcdfc75cb1770acab0da09d1140677a429615
Lavender Ayodi <layodi749@gmail.com>
2017-01-18 00:28:55 -0800

    Revised Fizz_Buzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,9 +1,13 @@
 def fizz_buzz(n):
-  if n%3 == 0:
-    return 'Fizz'
-  elif n%5 == 0:
-    return 'Buzz'
-  elif n%3 == 0 and n%5 == 0:
-    return 'FizzBuzz'
-  elif n%3 != 0 and n%5 != 0:
-      return n
+#for no.s divisible by both 3 and 5
+	if n%3 == 0 and n%5 == 0:
+		return 'FizzBuzz'
+#for no.s divisible by 3
+	elif n%3 == 0:
+		return 'Fizz'
+#for no.s divisible by 5
+	elif n%5 == 0:
+		return 'Buzz'
+	else:
+#no.s divisible by neither 3 or 5
+		return n
