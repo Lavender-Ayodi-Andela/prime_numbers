d40f960f949d582037d138b7f8a505551f0097f7
Lavender Ayodi <layodi749@gmail.com>
2017-01-19 02:03:13 -0800

    reduce code length

--- day_2_bootcamp/MaxMin/max_min.py
+++ day_2_bootcamp/MaxMin/max_min.py
@@ -2,17 +2,13 @@
 	#add minimum and maximum from list to min_max_list
 def find_max_min(num):
 	try:
-		min_max_list = []
 
 		for x in num:
-			min_max_list.append (min(num))
-			min_max_list.append (max(num))
-
 			minimum = min(num)
 			maximum = max(num)
 
 			if minimum != maximum:
-				return min_max_list
+				return [minimum,maximum]
 			else:
 				return [minimum]
 
