b6d4480e1e12ecf4821f49a909816accf6d7e73d
Lavender Ayodi <layodi749@gmail.com>
2017-01-19 01:08:29 -0800

    Correction done on min_max.py

--- day_2_bootcamp/MaxMin/max_min.py
+++ day_2_bootcamp/MaxMin/max_min.py
@@ -2,13 +2,11 @@
 	#add minimum and maximum from list to min_max_list
 def find_max_min(list):
 	try:
+		min_max_list = []
 
-	min_max_list = []
-
-	for list in find_max_min:
-		min_max_list.append (min (list))
-		min_max_list.append (max (list))
-
+		for list in find_max_min:
+		min_max_list.append (min(list))
+		min_max_list.append (max(list))
 		return min_max_list
 
 	except Exception as e:
