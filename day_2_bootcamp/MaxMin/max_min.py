7f11053688a944fd6ef82f5ed6b8af08b4ed39d7
Lavender Ayodi <layodi749@gmail.com>
2017-01-19 01:17:30 -0800

    changed indentation

--- day_2_bootcamp/MaxMin/max_min.py
+++ day_2_bootcamp/MaxMin/max_min.py
@@ -1,13 +1,13 @@
 	#new list to include minimum and maximum number
 	#add minimum and maximum from list to min_max_list
-def find_max_min(list):
+def find_max_min(num):
 	try:
 		min_max_list = []
 
-		for list in find_max_min:
-		min_max_list.append (min(list))
-		min_max_list.append (max(list))
-		return min_max_list
+		for x in num:
+			min_max_list.append (min(num))
+			min_max_list.append (max(num))
+			return min_max_list
 
 	except Exception as e:
 		return 'invalid entry'
