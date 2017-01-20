3b3f6fa6a82d93e9e541ca1d734d69480df6fb0f
Lavender Ayodi <layodi749@gmail.com>
2017-01-20 01:16:19 -0800

    rename folder

--- day_3_bootcamp/missingnumber/missing_number.py
+++ day_3_bootcamp/missingnumber/missing_number.py
@@ -1,9 +1,6 @@
 def find_missing(listone, listtwo):
-    #empty list
-    if len(listone)==0 and len(listtwo)==0:
-        return 0
-    #same entries
-    elif len(listone)==len(listtwo):
+    #returns zero for empty list and same entries
+    if len(listone)==0 and len(listtwo)==0 and len(listone)==len(listtwo):
         return 0
     #missing numbers
     else:
