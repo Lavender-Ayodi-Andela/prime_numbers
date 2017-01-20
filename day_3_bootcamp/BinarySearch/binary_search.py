5900c4eb9b395bc3eeabc78b60f13aada6f26bd2
Lavender Ayodi <layodi749@gmail.com>
2017-01-19 18:57:44 -0800

    add Binary Search Lab to Day 3 Folder

---
+++ day_3_bootcamp/BinarySearch/binary_search.py
@@ -0,0 +1,32 @@
+class BinarySearch(list):
+
+  def __init__(self,a,b):
+    super(BinarySearch, self).__init__(range(0, (a*b)+1, b)[1:])
+    self.length = a
+
+    def search(self,arg):
+        first = 0
+        last = len(self) - 1
+        found = False
+        count = 0
+        dict_ = {}
+        while not found and first <= last:
+            count +=1
+            midpoint = (first + last)//2
+            if self[last] == arg:
+                found = True
+            elif self[midpoint] > arg:
+                last = midpoint- 1
+            elif self[midpoint] < arg:
+                first = midpoint+ 1
+            else:
+                found = True
+                   
+            if first == last:
+                found = True
+
+                dict_[count] = count
+                dict_[index] = -1
+                return dict_
+
+	
